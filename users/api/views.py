4289fbae8f2968e8e534d3f3d4715741452d70ae
sariafba <sariafba@gmail.com>
2025-06-25 14:57:10 +0300

    EmployeePermission

--- users/api/views.py
+++ users/api/views.py
@@ -3,17 +3,39 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from users.models import Employee
 from .serializers import EmployeeSerializer
+from users.permissions import EmployeePermission
+from rest_framework.response import Response
+from rest_framework import status
 
 
 
 class EmployeeView(ModelViewSet):
 
-    # permission_classes = [IsAdminUser]
+    permission_classes = [EmployeePermission]
+    http_method_names = ['get', 'post', 'patch', 'delete']
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['role']
+
+    def get_queryset(self):
+
+        allowed_roles = {
+            'admin': ['cooperator', 'teacher', 'receptionist'],
+            'cooperator': ['teacher', 'receptionist'],
+            'teacher': [],
+            'receptionist': [],
+        
+        }
+        if self.request.user.is_superuser:
+            return self.queryset
+        
+        employee_role = self.request.user.employee.role
+        allowed_roles = allowed_roles.get(employee_role, [])
+        return self.queryset.filter(role__in=allowed_roles)
         
 
-    
+    def destroy(self, request, *args, **kwargs):
+        self.get_object().user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
---
+++ users/permissions.py
@@ -0,0 +1,85 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
+
+class EmployeePermission(BasePermission):
+
+    allowed_roles = {
+        'admin': ['cooperator', 'teacher', 'receptionist'],
+        'cooperator': ['teacher', 'receptionist'],
+        'teacher': [],
+        'receptionist': [],
+    }
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        if request.user.is_superuser:
+            return True
+
+        if not hasattr(request.user, 'employee'):
+            return False
+
+        employee_role = request.user.employee.role
+
+        if employee_role in ['teacher', 'receptionist']:
+            return False
+        
+        if request.method == 'GET':
+            return True
+
+        if request.method == 'POST':
+            input_role = request.data.get('role')
+
+            if not input_role or input_role not in self.allowed_roles:
+                return True # serializer will validate
+
+            if input_role in self.allowed_roles.get(employee_role, []):
+                return True
+            else:
+                raise PermissionDenied(f"You are not allowed to assign role '{input_role}'.")
+
+        # Allow PATCH/DELETE conditionally via has_object_permission
+        if request.method in ['PATCH', 'DELETE']:
+            return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        
+        if request.method == 'GET':
+            return True
+        
+        employee_role = request.user.employee.role
+        allowed_roles = self.allowed_roles.get(employee_role, [])
+        target_role = obj.role
+
+        if request.method == 'DELETE':
+            if target_role in allowed_roles:
+                return True
+            raise PermissionDenied(
+                f"You are not allowed to delete an employee with role '{target_role}'."
+            )
+
+        if request.method == 'PATCH':
+
+            # Also make sure the original role was allowed to be edited
+            if target_role not in allowed_roles:
+                raise PermissionDenied(
+                    f"You are not allowed to modify an employee with role '{target_role}'."
+                )
+            
+            new_role = request.data.get('role')
+
+            # If no role is changing or role stays the same, allow
+            if not new_role or new_role not in self.allowed_roles:
+                return True # serializer will validate
+
+            # If trying to change to an unassignable role
+            if new_role not in allowed_roles:
+                raise PermissionDenied(
+                    f"You are not allowed to change role to '{new_role}'."
+                )
+            return True
